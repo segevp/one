cd5196c54759100161fbff415f57bcda803305ef
segevpavin <macsegev@gmail.com>
2021-01-08 18:56:17 +0200

    functions inside soldier class

--- one.py
+++ one.py
@@ -6,7 +6,6 @@ from pprint import pprint
 ONE_API = "https://one.prat.idf.il/api"
 ONE_USER = ONE_API + "/account/getUser"
 ONE_REPORTED_DATA = ONE_API + "/Attendance/GetReportedData"
-ONE_MEMBER_HISTORY = ONE_API + "/Attendance/memberHistory"
 ONE_ATTEND = ONE_API + '/Attendance/InsertPersonalReport'
 
 RELEVANT_COOKIES = [
@@ -28,58 +27,53 @@ HEADERS = {
 }
 
 
-def load_cookies(path):
-    cookies = {}
-    with open(path, 'rb') as f:
-        try:
+class Soldier:
+    def __init__(self, path):
+        self.headers = HEADERS
+        self.path = path
+
+        self.cookies = self.load_cookies()
+        self.authenticated = self.authenticate()
+
+    def load_cookies(self):
+        with open(self.path, 'rb') as f:
             cookies = load(f)
+        if "AppCookie" not in cookies:
+            raise KeyError("Needed cookie not found")
+        return cookies if "AppCookie" in cookies else None
+
+    def _save_cookies(self):
+        cookies_to_save = {key: val for key, val in self.cookies.items() if key in SAVE_COOKIES}
+        with open(self.path, 'w') as f:
+            dump(cookies_to_save, f, indent=4)
+
+    def _update_cookies(self, new):
+        to_save = len({key: val for key, val in self.cookies.items() if key in SAVE_COOKIES}) != self.cookies
+        relevant_cookies = {key: val for key, val in new.items() if key in RELEVANT_COOKIES}
+        self.cookies.update(relevant_cookies)
+        if to_save:
+            self._save_cookies()
+
+    def authenticate(self):
+        try:
+            response = get(ONE_USER, cookies=self.cookies, headers=self.headers)
+            self._update_cookies(response.cookies)
+            return True
         except JSONDecodeError:
-            pass
-    return cookies if "AppCookie" in cookies else None
+            return False
 
+    def attend(self, main_code: str, secondary_code: str):
+        attendance_form = {
+            'MainCode': main_code,
+            'SecondaryCode': secondary_code
+        }
+        post(ONE_ATTEND, attendance_form, cookies=self.cookies, headers=self.headers)
 
-def authenticate(cookies, headers):
-    user_response = get(ONE_USER, cookies=cookies, headers=headers)
-    try:
-        return user_response
-    except JSONDecodeError:
-        pass
-
-
-def update_cookies(old, new):
-    relevant_cookies = {key: val for key, val in new.items() if key in RELEVANT_COOKIES}
-    old.update(relevant_cookies)
-
-
-def save_cookies(path, cookies):
-    cookies_to_save = {key: val for key, val in cookies.items() if key in SAVE_COOKIES}
-    with open(path, 'w') as f:
-        dump(cookies_to_save, f, indent=4)
-
-
-def attend(cookies, headers, main_code: str, secondary_code: str):
-    attendance_form = {
-        'MainCode': main_code,
-        'SecondaryCode': secondary_code
-    }
-    post(ONE_ATTEND, attendance_form, cookies=cookies, headers=headers)
-
-
-def reported_data(cookies, headers):
-    return get(ONE_REPORTED_DATA, cookies=cookies, headers=headers).json()
-
-
-def run(path, headers, main_code, secondary_code):
-    one_cookies = load_cookies(path)
-    user = authenticate(one_cookies, headers)
-    if not user:
-        print("--> ERROR: Authentication Failure.")
-        exit()
-    update_cookies(one_cookies, user.cookies)
-    save_cookies(path, one_cookies)
-    attend(one_cookies, headers, main_code, secondary_code)
-    pprint(reported_data(one_cookies, headers))
+    @property
+    def reported_data(self):
+        return get(ONE_REPORTED_DATA, cookies=self.cookies, headers=self.headers).json()
 
 
 if __name__ == '__main__':
-    run('config.json', HEADERS, '01', '01')
+    soldier = Soldier('config.json')
+    soldier.attend('01', '01')
